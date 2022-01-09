6e6d10644f530c9fd8adecd440f05cc42a812bd1
Responsibleuk <james@we-are-responsible.com>
2022-01-09 21:02:15 +0000

    Clean up code on end

--- assets/script/end.js
+++ assets/script/end.js
@@ -1,6 +1,5 @@
 /* jshint esversion: 8 */
 
-// const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
@@ -10,10 +9,6 @@ const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 const MAX_HIGH_SCORES = 100;
 
 finalScore.innerText = mostRecentScore;
-
-// username.addEventListener('keyup', () => {
-//     saveScoreBtn.disabled = !username.value
-// })
 
 saveHighScore = e => {
     e.preventDefault();
