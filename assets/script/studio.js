8476dbe0e75520b8f9f27aa614f61825897c5722
Responsibleuk <james@we-are-responsible.com>
2022-03-03 16:10:44 +0000

    Add comment for accordian

--- assets/script/studio.js
+++ assets/script/studio.js
@@ -1,5 +1,6 @@
 /* jshint esversion: 8 */
 
+// Accordian for expanding boxes on studios page
 const accordion_items_elms = document.querySelectorAll('.accordion .accordion-item');
 accordion_items_elms.forEach(accordionItem => {
   accordionItem.querySelector('.accordion-item-head').addEventListener('click', (e) => {
