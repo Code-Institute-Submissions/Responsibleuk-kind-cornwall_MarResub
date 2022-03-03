114315e863866c14e2b6b3e3870087ca4406dc24
Responsibleuk <james@we-are-responsible.com>
2022-03-03 16:25:46 +0000

    Add comment to nav.js

--- assets/script/nav.js
+++ assets/script/nav.js
@@ -1,5 +1,6 @@
 /* jshint esversion: 8 */
 
+// Create drop down nav bar for mobile devices with nav toggle
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".menu");
 navToggle.addEventListener("click", function () {
