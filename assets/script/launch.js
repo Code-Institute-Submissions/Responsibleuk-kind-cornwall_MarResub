b4599fa4882cf2faac9482916af870ae510edcea
Responsibleuk <james@we-are-responsible.com>
2022-03-03 13:44:34 +0000

    Add comments to launch.js

--- assets/script/launch.js
+++ assets/script/launch.js
@@ -2,7 +2,7 @@
 
 // Countdown
 
-
+// Defining the months for our countdown timer
 const months = [
   'January',
   'February',
@@ -17,6 +17,8 @@ const months = [
   'November',
   'December',
 ];
+
+// Defining the days for our countdown timer
 const weekdays = [
   'Sunday',
   'Monday',
@@ -26,11 +28,12 @@ const weekdays = [
   'Friday',
   'Saturday',
 ];
+
 const giveaway = document.querySelector('.date');
 const deadline = document.querySelector('.launch');
 const items = document.querySelectorAll('.launch-format h4');
- 
 
+// This sets the date that we are counting down to our launch date
 let launchDate = new Date(2022, 3, 25, 9, 30, 0);
 
 const year = launchDate.getFullYear();
@@ -41,19 +44,25 @@ let month = launchDate.getMonth();
 month = months[month];
 const weekday = weekdays[launchDate.getDay()];
 const date = launchDate.getDate();
+
 giveaway.textContent = `Launches ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
 
 const launchTime = launchDate.getTime();
 function getRemaindingTime() {
   const today = new Date().getTime();
 
+  /*
+  This section sets out the values for our days, hours, minutes, seconds, 
+  the values are in milliseconds, 1 day = 24 hours, 1 hour = 60 minutes, 1 minute = 60 seconds, 1 second = 1000 miliseconds  
+  */
+
   const t = launchTime - today;
- 
+
   const oneDay = 24 * 60 * 60 * 1000;
   const oneHour = 60 * 60 * 1000;
   const oneMinute = 60 * 1000;
-  
-  
+
+// This section calculates the values for seconds, minutes, hours and days
   let days = t / oneDay;
   days = Math.floor(days);
   let hours = Math.floor((t % oneDay) / oneHour);
@@ -79,4 +88,6 @@ function getRemaindingTime() {
 }
 
 let countdown = setInterval(getRemaindingTime, 1000);
+
+// finally we set the initial values for the countdown
 getRemaindingTime();
